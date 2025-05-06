0a7b51f5a9a4132f657f511acc60e74bae829146
Mike Gents <mikegents169@gmail.com>
2025-05-06 17:04:14 -0400

    MenuBar

--- src/main/java/com/example/practicetwo/HelloApplication.java
+++ src/main/java/com/example/practicetwo/HelloApplication.java
@@ -3,7 +3,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
@@ -25,12 +28,35 @@ public class HelloApplication extends Application {
         Exam myExam = new Exam(exam);
         myExam.printAllQuestions();
 
-        System.out.println("Hello World");
+        VBox root = new VBox();
+
+
+        MenuBar menuBarMain = buildMenuBar();
+        root.getChildren().add(menuBarMain);
+
+
+        HBox hboxGrade = new HBox();
+        hboxGrade.setAlignment(Pos.CENTER);
+        hboxGrade.getChildren().add(new Label("Grade:"));
+        root.getChildren().add(hboxGrade);
+
+
         Label helloWorldLabel = new Label("Hello World");
-        Scene scene = new Scene(helloWorldLabel, 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("ChampExamen (TM) application (C)");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private MenuBar buildMenuBar() {
+        MenuBar menuBarMain = new MenuBar();
+        Menu menuFile = new Menu("File");
+        Menu menuEdit = new Menu("Edit");
+        Menu menuQuiz = new Menu("Quiz");
+        Menu menuExtras = new Menu("Extras");
+        Menu menuHelp = new Menu("Help");
+        menuBarMain.getMenus().addAll(menuFile, menuEdit, menuQuiz, menuExtras, menuHelp);
+        return menuBarMain;
     }
 
     public static void main(String[] args) {
