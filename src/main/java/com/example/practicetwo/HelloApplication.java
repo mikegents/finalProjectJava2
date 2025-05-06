43f139e51651eeb9a0504dbf8da19c2f248a5f55
Mike Gents <mikegents169@gmail.com>
2025-05-06 17:17:50 -0400

    MenuBar

--- src/main/java/com/example/practicetwo/HelloApplication.java
+++ src/main/java/com/example/practicetwo/HelloApplication.java
@@ -3,14 +3,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.LinkedList;
 
@@ -38,14 +42,40 @@ public class HelloApplication extends Application {
         HBox hboxGrade = new HBox();
         hboxGrade.setAlignment(Pos.CENTER);
         hboxGrade.getChildren().add(new Label("Grade:"));
+
+
+        HBox hBoxBanner = buildBanner();
+        root.getChildren().add(hBoxBanner);
+
         root.getChildren().add(hboxGrade);
+        root.getChildren().add(new Separator());
+
+
 
 
         Label helloWorldLabel = new Label("Hello World");
-        Scene scene = new Scene(root, 320, 240);
+        Scene scene = new Scene(root, 800, 800);
         stage.setTitle("ChampExamen (TM) application (C)");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private HBox buildBanner() {
+        Image logoObj = new Image("/logo.png");
+        ImageView imageViewLogo = new ImageView(logoObj);
+        imageViewLogo.setPreserveRatio(true);
+        imageViewLogo.setFitHeight(100);
+        Image bannerObj = new Image("/banner.png");
+        ImageView imageViewBanner = new ImageView(bannerObj);
+        imageViewBanner.setPreserveRatio(true);
+        imageViewBanner.setFitHeight(100);
+        HBox hboxBanner = new HBox();
+        hboxBanner.getChildren().addAll(imageViewLogo, imageViewBanner);
+        return hboxBanner;
+    }
+
+    private Hbox buildFooter() {
+
     }
 
     private MenuBar buildMenuBar() {
