84d04910b03d3c3e52c79bf8a542d50ae25a1593
Mike Gents <mikegents169@gmail.com>
2025-05-08 11:45:43 -0400

    added questions to gooey

--- src/main/java/com/example/practicetwo/HelloApplication.java
+++ src/main/java/com/example/practicetwo/HelloApplication.java
@@ -1,5 +1,7 @@
 package com.example.practicetwo;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -14,9 +16,15 @@ import javafx.scene.image.Image;
 import javafx.scene.control.Button;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 public class HelloApplication extends Application {
+    private Exam myExam;
     @Override
     public void start(Stage stage) throws IOException {
 
@@ -24,28 +32,37 @@ public class HelloApplication extends Application {
         myBank.readMCQ("src/main/resources/mcq.txt");
         myBank.readTFQ("src/main/resources/tfq.txt");
 
+        List<Integer> indxesList = new ArrayList<>(Arrays.asList(11, 0, 5, 10, 9, 8, 7, 6));
 
-        int[] indxes = {11, 0, 5};
+
+        for (int i = 0; i < 3; i++) {
+            int randomIndex = ThreadLocalRandom.current().nextInt(1, 15); // 15 is exclusive
+            indxesList.add(randomIndex);
+        }
+
+        int[] indxes = indxesList.stream().mapToInt(Integer::intValue).toArray();
         LinkedList<Question> exam = myBank.selectRandomQuestion(indxes);
-        Exam myExam = new Exam(exam);
+        myExam = new Exam(exam);
         myExam.printAllQuestions();
 
         VBox root = new VBox();
 
-
         MenuBar menuBarMain = buildMenuBar();
         root.getChildren().add(menuBarMain);
-
 
         HBox hboxGrade = new HBox();
         hboxGrade.setAlignment(Pos.CENTER);
         hboxGrade.getChildren().add(new Label("Grade:"));
+
+        VBox[] vboxQuestions = buildQuestionsVboxes();
 
 
         HBox hBoxBanner = buildBanner();
         root.getChildren().add(hBoxBanner);
 
         root.getChildren().add(hboxGrade);
+        root.getChildren().add(new Separator());
+        root.getChildren().addAll(vboxQuestions);
         root.getChildren().add(new Separator());
 
         HBox hBoxButtons = buildFooter();
@@ -60,9 +77,51 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    private VBox[] buildQuestionsVboxes() {
+        int numberOfQuestionsInExam = myExam.questions.size();
+        VBox[] vBoxes = new VBox[numberOfQuestionsInExam];
+
+        for (int i = 0; i<numberOfQuestionsInExam; i++){
+            Question question = myExam.getQuestion(i+1);
+            if (question.getQuestionType()==questionType.TFQ){
+                vBoxes[i] = buildTrueFalseQ(1, (TFQQuestion) question);
+            } else { // it is MCQ
+                vBoxes[i] = buildMCQ(2, (MCQuestion) question);
+            }
+        }
+        return vBoxes;
+    }
+
+    public VBox buildTrueFalseQ(int questionNumber, TFQQuestion tfQuestion1){
+        Label labelQuestionText = new Label(tfQuestion1.getQuestionText());
+        RadioButton radioButtonTrue = new RadioButton("True ");
+        RadioButton radioButtonFalse = new RadioButton("False");
+        HBox hBox = new HBox(10, radioButtonTrue, radioButtonFalse);
+        VBox vBox = new VBox(labelQuestionText, hBox);
+        vBox.getChildren().add(new Separator());
+        return vBox;
+    }
+
+    public VBox buildMCQ(int questionNumber, MCQuestion mcqQuestion){
+        Label labelQuestionText = new Label(mcqQuestion.getQuestionText());
+        VBox vBox = new VBox(labelQuestionText);
+
+        LinkedList<String> options = mcqQuestion.getOptions();
+        ToggleGroup toggleGroup = new ToggleGroup();
+        for (String s : options){
+            RadioButton radioButton = new RadioButton(s);
+            radioButton.setToggleGroup(toggleGroup);
+            vBox.getChildren().add(radioButton);
+
+        }
+        vBox.getChildren().add(new Separator());
+        return vBox;
+    }
+
     private HBox buildBanner() {
         Image logoObj = new Image("/logo.png");
         ImageView imageViewLogo = new ImageView(logoObj);
+
         imageViewLogo.setPreserveRatio(true);
         imageViewLogo.setFitHeight(100);
         Image bannerObj = new Image("/banner.png");
@@ -75,12 +134,24 @@ public class HelloApplication extends Application {
     }
 
     private HBox buildFooter() {
-        HBox hboxFooter = new HBox();
+        HBox hboxFooter = new HBox(10);
         Button buttonClear = new Button("Clear");
-        Button ButtonSave = new Button("Save");
+        Button buttonSave = new Button("Save");
         Button buttonSubmit = new Button("Submit");
-        hboxFooter.getChildren().addAll(buttonClear, ButtonSave, buttonSubmit);
+
+        // register to actions
+        buttonClear.setOnAction(e -> clearExamAnswers());
+        buttonSave.setOnAction(e -> saveExamAnswers());
+        buttonSubmit.setOnAction(new SubmitEventHandler());
+
+        hboxFooter.getChildren().addAll(buttonClear, buttonSave, buttonSubmit);
         return hboxFooter;
+    }
+
+    private void saveExamAnswers() {
+    }
+
+    private void clearExamAnswers() {
     }
 
     private MenuBar buildMenuBar() {
@@ -96,5 +167,15 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+
+    }
+    public static class SubmitEventHandler implements EventHandler<ActionEvent> {
+
+
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            System.out.println("Submit button clicked");
+        }
     }
 }
+
